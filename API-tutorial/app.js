8537de389c5872866cfd1919728a8087bde0c4b0
kavitarawatdev <ikavitarawat227@gmail.com>
2025-07-08 23:34:31 +0530

    add files via upload

--- API-tutorial/app.js
+++ API-tutorial/app.js
@@ -17,7 +17,7 @@ app.use("/api/products",products_routes )
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT, () => {
+        app.listen(PORT,'0.0.0.0', () => {
             console.log(`Server running at http://localhost:${PORT}`);
         });
     } catch (error) {
